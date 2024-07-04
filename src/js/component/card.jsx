f0526920599623a3b75227f6205e155f338eef4d
menurux <jorgepino.utu@gmail.com>
2024-07-04 15:48:40 -0300

    modified card

--- src/js/component/card.jsx
+++ src/js/component/card.jsx
@@ -9,7 +9,7 @@ const Card = () => {
     return (
         <div className="d-flex gap-4">
             <div className="card" style={{ width: "18rem" }}>
-                <img src={rigoImage} className="card-img-top" alt="Rigo Baby" />
+                <img src="https://loremflickr.com/400/400" className="card-img-top" alt="Rigo Baby" />
                 <div className="card-body">
                     <h5 className="card-title">Card title</h5>
                     <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
